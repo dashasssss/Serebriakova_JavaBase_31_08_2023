87a8ff6a4b49b45c61212383de8da9607eb7f584
Daria Serebriakova <[  ]>
2023-09-10 23:18:34 +0300

    text change

--- src/main/java/ua/hillel/serebriakova/homeworks/homework3/Main.java
+++ src/main/java/ua/hillel/serebriakova/homeworks/homework3/Main.java
@@ -2,6 +2,6 @@ package ua.hillel.serebriakova.homeworks.homework3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привіт Світ!");
     }
 }
