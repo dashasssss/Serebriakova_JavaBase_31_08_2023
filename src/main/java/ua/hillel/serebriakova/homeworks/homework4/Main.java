e4910dfe95c37a1293be60753089a7723d9a526e
Daria Serebriakova <[  ]>
2023-09-18 14:05:37 +0300

    text change

--- src/main/java/ua/hillel/serebriakova/homeworks/homework4/Main.java
+++ src/main/java/ua/hillel/serebriakova/homeworks/homework4/Main.java
@@ -1,2 +1,18 @@
-package ua.hillel.serebriakova.homeworks.homework4;public class Main {
+package ua.hillel.serebriakova.homeworks.homework4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int length = 25;
+        int width = 16;
+        int height = 17;
+
+        int volume = length * width * height;
+
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+
+        int totalLength = 4 * (length + width + height);
+
+        System.out.println("Сумарна довжина всіх сторін = " + totalLength);
+    }
 }
