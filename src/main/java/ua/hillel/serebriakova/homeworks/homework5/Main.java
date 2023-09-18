4b4652f40df104ecc2f6066646925ab3cc5f90fd
Daria Serebriakova <[  ]>
2023-09-19 00:46:36 +0300

    add text

--- src/main/java/ua/hillel/serebriakova/homeworks/homework5/Main.java
+++ src/main/java/ua/hillel/serebriakova/homeworks/homework5/Main.java
@@ -1,4 +1,22 @@
 package ua.hillel.serebriakova.homeworks.homework5;
 
 public class Main {
+    public static void main(String[] args) {
+        int attackWarriorLi = 13;
+        int attackArcherLi = 24;
+        int attackHorsemanLi = 46;
+
+        int attackWarriorMin = 9;
+        int attackArcherMin = 35;
+        int attackHorsemanMin = 12;
+
+        int numWarriorsLi = 860;
+        int numArchersLi = 860;
+        int numHorsemenLi = 860;
+
+        int totalAttackLi = (attackWarriorLi * numWarriorsLi) + (attackArcherLi * numArchersLi) + (attackHorsemanLi * numHorsemenLi);
+
+        System.out.println("Загальний показник атаки династії Лі: " + totalAttackLi);
+        //System.out.println("Загальний показник атаки династії Мінь: " + totalAttackMin);
+    }
 }
