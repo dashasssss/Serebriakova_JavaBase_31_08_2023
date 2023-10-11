94b4fb8f9a3f4414d6b77d092aee513d045d461c
Daria Serebriakova <[  ]>
2023-10-12 00:13:42 +0300

    add text

--- src/main/java/ua/hillel/serebriakova/homeworks/homework/Person.java
+++ src/main/java/ua/hillel/serebriakova/homeworks/homework/Person.java
@@ -6,42 +6,48 @@ public class Person {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String firstName = "";
-        String lastName = "";
-        String phoneNumber = "";
-        String city = "";
+        String personFirstName = "";
+        String personLastName = "";
+        String personPhoneNumber = "";
+        String personCity = "";
 
-        while (firstName.isEmpty()) {
+        while (personFirstName.isEmpty()) {
             System.out.print("Введіть ім'я: ");
-            firstName = scanner.nextLine().trim();
-            if (firstName.isEmpty()) {
+            personFirstName = scanner.nextLine().trim();
+            if (personFirstName.isEmpty()) {
                 System.out.println("Ім'я не може бути пустим. Будь ласка, введіть ще раз.");
             }
         }
 
 
-        while (lastName.isEmpty()) {
+        while (personLastName.isEmpty()) {
             System.out.print("Введіть призвіще: ");
-            lastName = scanner.nextLine().trim();
-            if (lastName.isEmpty()) {
+            personLastName = scanner.nextLine().trim();
+            if (personLastName.isEmpty()) {
                 System.out.println("Призвіще не може бути пустим. Будь ласка, введіть ще раз.");
             }
         }
 
-        while (phoneNumber.isEmpty()) {
+        while (personPhoneNumber.isEmpty()) {
             System.out.print("Введіть номер телефона: ");
-            phoneNumber = scanner.nextLine().trim();
-            if (phoneNumber.isEmpty()) {
+            personPhoneNumber = scanner.nextLine().trim();
+            if (personPhoneNumber.isEmpty()) {
                 System.out.println("Номер телефону не може бути пустим. Будь ласка, введіть ще раз.");
             }
         }
 
-        while (city.isEmpty()) {
+        while (personCity.isEmpty()) {
             System.out.print("Введіть місто: ");
-            city = scanner.nextLine().trim();
-            if (city.isEmpty()) {
+            personCity = scanner.nextLine().trim();
+            if (personCity.isEmpty()) {
                 System.out.println("Місто не може бути пустим. Будь ласка, введіть ще раз.");
             }
         }
+        personInfo(personFirstName,personLastName,personCity , personPhoneNumber);
+
+    }
+
+    static void personInfo(String name, String  lastName, String city, String tel) {
+        System.out.println("Зателефонувати громадянину "+name + " " + lastName+ " із міста "+ city +" можна за номером "+ tel);
     }
 }
