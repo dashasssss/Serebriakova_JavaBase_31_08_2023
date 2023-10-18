9fcc13f9a579d76c272fa1f23c6d5499f6eae8b1
Daria Serebriakova <[  ]>
2023-10-19 02:36:54 +0300

    add text

--- src/main/java/ua/hillel/serebriakova/homeworks/homework14/FitnessUser.java
+++ src/main/java/ua/hillel/serebriakova/homeworks/homework14/FitnessUser.java
@@ -1,5 +1,73 @@
 package ua.hillel.serebriakova.homeworks.homework14;
 
 public class FitnessUser {
+    private  String firstName;
+    private int birthDay;
+    private int birthMonth;
+    private int birthYear;
+    private String email;
+    private  String phone;
+    private String lastName;
+    private double weight;
+    private String bloodPressure;
+    private int dailySteps;
+    private int age;
 
+    public FitnessUser(String firstName, int birthDay, int birthMonth, int birthYear,
+                       String email, String phone, String lastName, double weight, String bloodPressure,
+                       int dailySteps) {
+        this.firstName = firstName;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+        this.email = email;
+        this.phone = phone;
+        this.lastName = lastName;
+        this.weight = weight;
+        this.bloodPressure = bloodPressure;
+        this.dailySteps = dailySteps;
+        calculateAge();
+
+    }
+
+    private void calculateAge() {
+        age = 2023 - birthYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public int getDailySteps() {
+        return dailySteps;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void printAccountInfo() {
+        System.out.println("Account Information:");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Blood Pressure: " + bloodPressure);
+        System.out.println("Daily Steps: " + dailySteps);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("------------------------------");
+    }
 }
--- src/main/java/ua/hillel/serebriakova/homeworks/homework14/Main.java
+++ src/main/java/ua/hillel/serebriakova/homeworks/homework14/Main.java
@@ -2,6 +2,20 @@ package ua.hillel.serebriakova.homeworks.homework14;
 
 public class Main {
     public static void main(String[] args) {
+        FitnessUser user1 = new FitnessUser("John", 15, 5, 1990, "john@example.com", "097456789", "Doe", 75.5, "120/80", 8000);
 
+        FitnessUser user2 = new FitnessUser("Alice", 10, 8, 1985, "alice@example.com", "097654321", "Smith", 68.2, "110/70", 10000);
+
+        FitnessUser user3 = new FitnessUser("Ann", 2, 2, 2004, "ann@example.com", "098356787", "Jones", 53.2, "110/70", 12000);
+
+        FitnessUser user4 = new FitnessUser("Griffin", 27, 1, 2009, "griffin@example.com", "098996745", "Miller", 78.9, "110/70", 5000);
+
+        FitnessUser user5 = new FitnessUser("Hayes", 9, 9, 1977, "hayes@example.com", "098676689", "Lee", 97.5, "110/70", 8000);
+
+        user1.printAccountInfo();
+        user2.printAccountInfo();
+        user3.printAccountInfo();
+        user4.printAccountInfo();
+        user5.printAccountInfo();
     }
 }
