3e5a606a30c626f2175a8c42b31cda8582ce350d
Daria Serebriakova <[  ]>
2023-11-03 11:38:22 +0200

    add text

--- src/main/java/ua/hillel/serebriakova/homeworks/homework18/Main.java
+++ src/main/java/ua/hillel/serebriakova/homeworks/homework18/Main.java
@@ -1,7 +1,30 @@
 package ua.hillel.serebriakova.homeworks.homework18;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public double calculateAverage(int[] array) {
+        if (array.length == 0) {
+            return 0.0;
+        }
+
+        double sum = 0;
+        for (int value : array) {
+            sum += value;
+        }
+
+        return sum / array.length;
+    }
+
+    public boolean isSquareMatrix(int[][] matrix) {
+        int rows = matrix.length;
+
+        for (int[] row : matrix) {
+            if (row.length != rows) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
+
